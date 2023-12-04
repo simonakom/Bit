1176a0ef4cbd3be56e236375a412683916a7d4e9
simonakomarovska <simonak399@gmail.com>
2023-12-04 17:52:57 +0100

    JS Loop

--- 2023.12.04 (JS Loop)/Loop/mok1.js
+++ 2023.12.04 (JS Loop)/Loop/mok1.js
@@ -22,3 +22,46 @@ for (let indeksas = 0; indeksas < n; indeksas++) {
 	eilute += "<br>";
 	document.write(eilute);
 }
+
+
+
+
+// // Uppercase names
+
+// let people = [`ema`, `ona`, `mia`];
+// let x = 0;
+// console.log(people)
+
+// while (x<3) {
+// 	console.log(people[x].toUpperCase());
+// 	x++;
+// }
+
+
+// // // Uppercase names (+lenght)
+
+// let ppl = ["Petras", "Rasa", "Jonas", "Justinas", "Jurate", "Kastytis"];
+// let b = 0;
+// console.log(ppl)
+
+// while (b<ppl.length) {
+// 	console.log(ppl[b].toUpperCase());
+// 	b++;
+// }
+
+
+// // kas 4 skaiciai iki 100
+
+// for (let s=0; s<1000; s+=4){
+// 	console.log (s)
+// }
+
+
+// // lyginiai skaicia iki 10
+
+// for (let h=0; h<10; h++){
+// 	if (h%2===0){
+// 		console.log(h)
+// 	}
+// }
+
